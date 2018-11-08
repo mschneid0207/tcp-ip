b94decea950c826f66b411c19f170c4dbeb42b6d
Markus Schneider <markus.schneider0207@gmail.com>
2018-11-08 21:37:33 +0100

    initial commit

---
+++ src/de/mschneid/tcpip/GreetClientTest.java
@@ -0,0 +1,18 @@
+package de.mschneid.tcpip;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GreetClientTest {
+
+    @Test
+    public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() throws IOException {
+        GreetClient client = new GreetClient();
+        client.startConnection("127.0.0.1", 6666);
+        String response = client.sendMessage("hello server");
+        assertEquals("hello client", response);
+    }
+}
